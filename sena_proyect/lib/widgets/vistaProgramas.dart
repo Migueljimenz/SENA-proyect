ea26bc9cb79506cbb0efdc12fb88f47c0ac742cd
Migueljimenz <migueljiarteta@gmail.com>
2022-03-03 09:39:31 -0500

    se arreglo el Scroll en la descripcion de programa

--- sena_proyect/lib/widgets/vistaProgramas.dart
+++ sena_proyect/lib/widgets/vistaProgramas.dart
@@ -54,38 +54,44 @@ class Programas extends StatelessWidget {
                                 child: Text('Descripcion del programa')),
                           ),
                           backgroundColor: Colors.white,
-                          body: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(20.0),
-                                child: Container(
-                                  height: 100,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Colors.black,
-                                      width: 2,
-                                    ),
-                                  ),
+                          body: SingleChildScrollView(
+                            child: Column(
+                              children: <Widget>[
+                                Center(
                                   child: Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: Center(
-                                      child: Text(pro[index].programa,
-                                          style: const TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                          )),
+                                    padding: const EdgeInsets.only(
+                                        top: 20, right: 20, left: 20),
+                                    child: Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      height: 100,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: Border.all(
+                                            color: Colors.orange, width: 2),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Center(
+                                          child: Text(
+                                            pro[index].programa,
+                                            style: const TextStyle(
+                                              fontSize: 30,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Container(
-                                child: descProgramas(
-                                  descripcion: descripcion,
+                                // se llama la funcion que contiene los datos de la descripcion del programa
+                                Container(
+                                  child: descProgramas(
+                                    descripcion: descripcion,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ));
                     }),
                   );
@@ -98,6 +104,7 @@ class Programas extends StatelessWidget {
     );
   }
 
+// se crea una funcion con la lista de los programas y se le asigna una lista de las descripciones de cada programa para poder mostrarlo en la pantalla
   descProgramas({required List<DesPrograma> descripcion}) {
     return SingleChildScrollView(
       child: Column(
@@ -108,20 +115,20 @@ class Programas extends StatelessWidget {
             itemCount: descripcion.length,
             itemBuilder: (context, index) {
               return Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.only(top: 5, right: 10, left: 10),
                 child: Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: Colors.white,
                     border: Border.all(
-                      color: Colors.black,
+                      color: Colors.orange,
                       width: 2,
                     ),
                   ),
                   child: ListTile(
                     title: Text(descripcion[index].descPrograma,
                         style: const TextStyle(
-                          fontSize: 10,
+                          fontSize: 20,
                           fontWeight: FontWeight.bold,
                         )),
                   ),
